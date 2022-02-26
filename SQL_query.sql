c398b85c8f21b682aac95c30d4b0d7f552e83d6b
NoeliaZam21 <81967988+NoeliaZam21@users.noreply.github.com>
2022-02-26 00:06:09 -0600

    Add files via upload

---
+++ SQL_query.sql
@@ -0,0 +1,25 @@
+select * from dim_albums;
+
+select * from dim_artists;
+
+select * from dim_customers;
+
+select * from dim_invoice_items;
+
+select * from dim_location;
+
+select * from dim_playlists;
+
+select * from dim_tracks;
+
+select * from dim_time;
+
+---delete FROM dim_time;
+
+select * from Fact_invoice_item;
+
+---delete FROM Fact_invoice_item;
+--DROP TABLE Fact_invoice_item;
+
+
+
